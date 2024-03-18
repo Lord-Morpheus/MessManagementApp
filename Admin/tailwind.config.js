71f2829e3f60655385216b218961c2133448c7b6
Rudra-IITM <singhrudra5556@gmail.com>
2024-03-18 23:11:45 +0530

    tailwind config change

--- Admin/tailwind.config.js
+++ Admin/tailwind.config.js
@@ -1,13 +1,25 @@
-const {
+import {
   default: flattenColorPalette,
-} = require("tailwindcss/lib/util/flattenColorPalette");
+} from "tailwindcss/lib/util/flattenColorPalette";
+// export default {
+//   content: [
+
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
@@ -20,7 +32,7 @@ module.exports = {
 };
 
 // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
-function addVariablesForColors({ addBase, theme }: any) {
+function addVariablesForColors({ addBase, theme }) {
   let allColors = flattenColorPalette(theme("colors"));
   let newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
