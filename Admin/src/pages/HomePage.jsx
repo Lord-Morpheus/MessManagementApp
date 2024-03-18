28936e5b9c35a98392a641fef8c291210868c770
Lord-Morpheus <tarurak4@gmail.com>
2024-03-18 15:32:42 +0530

    update

--- Admin/src/pages/HomePage.jsx
+++ Admin/src/pages/HomePage.jsx
@@ -3,11 +3,11 @@ import Sidebar from "./sidebar";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-9 relative">
+    <div className="grid grid-cols-10 relative">
       <aside className="self-start col-span-2 ">
         <Sidebar/>
       </aside>
-      <div className="col-span-7 bg-aliceblue">
+      <div className="col-span-8 bg-aliceblue">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae tenetur
         ipsam impedit! Corrupti quia cupiditate maiores eligendi tenetur, magnam
         placeat odit quasi ea doloremque ullam vitae ad a. Reprehenderit itaque
--- Admin/src/pages/MessDetail.jsx
+++ Admin/src/pages/MessDetail.jsx
@@ -1,26 +1,66 @@
 import React from "react";
-import Sidebar from './sidebar';
-import Eating1 from '../images/eatingImg1.svg';
-import Eating2 from '../images/eatingImg2.svg';
+import Sidebar from "./sidebar";
+import Eating1 from "../images/eatingImg1.svg";
+import Eating2 from "../images/eatingImg2.svg";
 export default function Mess() {
   return (
-    <div className="grid grid-cols-9 relative">
+    <div className="grid grid-cols-10 relative">
       <aside className="self-start col-span-2 ">
-        <Sidebar/>
+        <Sidebar />
       </aside>
-      <div className="col-span-7 bg-aliceblue">
-        
-        <div className="flex justify-center bg-darkPeach">
-          <img className="object-fill h-48 w-96" src={Eating1} alt="not found" />
-          <img className="object-fill h-48 w-96" src={Eating2} alt="not found" />
-          <img className="object-fill h-48 w-96" src={Eating1} alt="not found" />
+      <div className="col-span-8 flex justify-center items-center bg-aliceblue">
+        <div className="bg-white h-5/6 w-5/6 rounded-3xl shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
+          <div className="flex justify-center bg-darkPeach rounded-3xl">
+            <img
+              className="object-fill h-48 w-96"
+              src={Eating1}
+              alt="not found"
+            />
+            <img
+              className="object-fill h-48 w-96"
+              src={Eating2}
+              alt="not found"
+            />
+            <img
+              className="object-fill h-48 w-96"
+              src={Eating1}
+              alt="not found"
+            />
+          </div>
+          <div className="navbar bg-gray-400 rounded-box">
+            <div className="flex-1 px-2 lg:flex-none">
+              <a className="text-lg font-bold">daisyUI</a>
+            </div>
+            <div className="flex justify-end flex-1 px-2">
+              <div className="flex items-stretch">
+                <a className="btn btn-ghost rounded-btn">Button</a>
+                <div className="dropdown dropdown-end">
+                  <div
+                    tabIndex={0}
+                    role="button"
+                    className="btn btn-ghost rounded-btn"
+                  >
+                    Dropdown
+                  </div>
+                  <ul
+                    tabIndex={0}
+                    className="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4"
+                  >
+                    <li>
+                      <a>Item 1</a>
+                    </li>
+                    <li>
+                      <a>Item 2</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <li>search filter</li>
+          <li>mess menu upload</li>
+          <li>footer</li>
         </div>
-        <div>
-          card
-        </div>
-        <li>search filter</li>
-        <li>mess menu upload</li>
-        <li>footer</li>
       </div>
     </div>
   );
