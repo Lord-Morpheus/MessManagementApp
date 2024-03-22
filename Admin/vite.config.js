b9be8c786a358102ff1adc3d34c12ba18fbd307c
Rudra-IITM <singhrudra5556@gmail.com>
2024-03-22 13:12:31 +0530

    change vite conf

--- Admin/vite.config.js
+++ Admin/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
+export default defineConfig(() => {
+  return {
+    define: {
+      // eslint-disable-next-line no-undef
+      __APP_ENV__: process.env.VITE_BACKEND_URI,
+    },
+    plugins: [react()],
+  };
+});
