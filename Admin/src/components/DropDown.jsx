5db7a34c16784024937ba3c02383fea81022ea60
Lord-Morpheus <tarurak4@gmail.com>
2024-05-02 12:48:22 +0530

    mess bill page added

--- Admin/src/components/DropDown.jsx
+++ Admin/src/components/DropDown.jsx
@@ -78,28 +78,20 @@ export const DropDown = ({ username, name, email }) => {
           className="py-2 text-sm text-gray-700 dark:text-gray-200"
           aria-labelledby="dropdownInformationButton"
         >
-          {/* <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Dashboard
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Settings
-            </a>
-          </li> */}
           <li>
             <Link
               className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
               to={'/messSelect'}
             >
               Release Form
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              to={'/bill'}
+            >
+              Bill Generated
             </Link>
           </li>
         </ul>
